b09047806e5f5680734b55a6849720885fa160fe
mattmolinare <mattmolinare@gmail.com>
2021-11-17 19:28:41 -0800

    refactor: create data subfolder

--- data/brain-tumor-public-dataset/download.sh
+++ data/brain-tumor-public-dataset/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+outdir=$1
 tmpfile=$(mktemp)
 wget https://figshare.com/ndownloader/articles/1512427/versions/5 -O $tmpfile
-outdir=mat
 unzip $tmpfile -d $outdir
 rm $tmpfile
 for file in $outdir/*.zip; do
