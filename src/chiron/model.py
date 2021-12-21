5152edaf7ac19d342da5297029282f568f72e3c3
mattmolinare <mattmolinare@gmail.com>
2021-12-20 19:01:35 -0800

    fix: make serializable
    Make serializable so model JSON file can be loaded.
    
    Remove dropout layer.

--- src/chiron/model.py
+++ src/chiron/model.py
@@ -4,14 +4,18 @@ import tensorflow_hub as hub
 __all__ = ["get_model"]
 
 
-def get_model(num_classes, dropout_rate=0, regularizer=None):
+@tf.keras.utils.register_keras_serializable()
+class KerasLayer(hub.KerasLayer):
+    pass
+
+
+def get_model(num_classes, regularizer=None):
     return tf.keras.Sequential(
         [
-            hub.KerasLayer(
+            KerasLayer(
                 "https://tfhub.dev/google/imagenet/efficientnet_v2_imagenet1k_b0/feature_vector/2",
                 trainable=True,
             ),
-            tf.keras.layers.Dropout(dropout_rate),
             tf.keras.layers.Dense(num_classes, kernel_regularizer=regularizer),
         ]
     )
